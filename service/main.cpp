2bf2b3716bb96cc89369c35faf4b2282fd916c95
Kyrylo Polezhaiev <kirushyk@gmail.com>
2021-01-20 02:26:30 +0200

    service: Add HTTP server

--- service/main.cpp
+++ service/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <libsoup/soup.h>
 #include <gst/gst.h>
 #include <gst/app/gstappsink.h>
+
+const int port = 8080;
 
 static gboolean on_message(GstBus *bus, GstMessage *message, gpointer user_data)
 {
@@ -31,6 +34,7 @@ static GstFlowReturn appsink_new_sample(GstAppSink *appsink, gpointer user_data)
 
 int main(int argc, char *argv[])
 {
+    SoupServer *http_server = soup_server_new(SOUP_SERVER_SERVER_HEADER, "HLS Demo ", NULL);
     gst_init(&argc, &argv);
     GstElement *pipeline = gst_pipeline_new(NULL);
     GstElement *videotestsrc = gst_element_factory_make("videotestsrc", NULL);
@@ -38,6 +42,7 @@ int main(int argc, char *argv[])
     GstElement *timeoverlay = gst_element_factory_make("timeoverlay", NULL);
     GstElement *videoconvert = gst_element_factory_make("videoconvert", NULL);
     GstElement *h264enc = gst_element_factory_make("vtenc_h264", NULL);
+    g_object_set(h264enc, "realtime", TRUE, NULL);
     GstElement *mp4mux = gst_element_factory_make("mp4mux", NULL);
     g_object_set(mp4mux, "faststart", TRUE, "fragment-duration", 200, NULL);
     GstElement *sink = gst_element_factory_make("appsink", NULL);
@@ -46,12 +51,17 @@ int main(int argc, char *argv[])
     gst_bin_add_many(GST_BIN(pipeline), videotestsrc, timeoverlay, videoconvert, h264enc, mp4mux, sink, NULL);
     gst_element_link_many(videotestsrc, timeoverlay, videoconvert,  h264enc, mp4mux, sink, NULL);
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (soup_server_listen_all(http_server, port, static_cast<SoupServerListenOptions>(0), 0))
+        g_print("server listening on port %d\n", port);
+    else
+        g_printerr("port %d could not be bound\n", port);
     GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     GstBus *bus = gst_element_get_bus(pipeline);
     gst_bus_add_watch(bus, on_message, main_loop);
     gst_object_unref(bus);
     g_main_loop_run(main_loop);
     g_main_loop_unref(main_loop);
+    soup_server_disconnect(http_server);
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(pipeline);
     return 0;
