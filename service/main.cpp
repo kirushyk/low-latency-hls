2cfd6052827822b6491387bed2ac52a9dd0a892e
Kyrylo Polezhaiev <kirushyk@gmail.com>
2021-01-21 06:06:18 +0200

    service: Implement storing of HLS output

--- service/main.cpp
+++ service/main.cpp
@@ -1,3 +1,6 @@
+#include <cstdint>
+#include <vector>
+#include <map>
 #include <iostream>
 #include <libsoup/soup.h>
 #include <gst/gst.h>
@@ -18,7 +21,64 @@ static gboolean on_message(GstBus *bus, GstMessage *message, gpointer user_data)
     return TRUE;
 }
 
-static GstFlowReturn appsink_new_sample(GstAppSink *appsink, gpointer user_data)
+struct HLSSegment
+{
+    int number;
+    GstBuffer *buffer;
+    GstMapInfo mapInfo;
+    GstSample *sample;
+};
+
+#define SEGMENTS_COUNT 5
+
+struct HLSOutput
+{
+    int lastIndex, lastSegment;
+    HLSSegment segments[SEGMENTS_COUNT];
+public:
+    HLSOutput();
+    void pushSegment(GstSample *sample);
+};
+
+HLSOutput::HLSOutput()
+{
+    lastIndex = 0;
+    lastSegment = 0;
+    for (int i = 0; i < SEGMENTS_COUNT; i++)
+    {
+        segments[i].buffer = NULL;
+        segments[i].sample = NULL;
+    }
+}
+
+void HLSOutput::pushSegment(GstSample *sample)
+{
+    if (segments[lastIndex].buffer)
+        gst_buffer_unmap(segments[lastIndex].buffer, &segments[lastIndex].mapInfo);
+    if (segments[lastIndex].sample)
+        gst_sample_unref(segments[lastIndex].sample);
+    segments[lastIndex].buffer = gst_sample_get_buffer(sample);
+    gst_buffer_map(segments[lastIndex].buffer, &segments[lastIndex].mapInfo, (GstMapFlags)(GST_MAP_READ));
+    
+    lastIndex++;
+    if (lastIndex >= SEGMENTS_COUNT)
+    {
+        lastIndex = 0;
+    }
+    std::cerr << "fmp4.";
+}
+
+static GstFlowReturn mp4sink_new_sample(GstAppSink *appsink, gpointer user_data)
+{
+    HLSOutput *hlsOutput = reinterpret_cast<HLSOutput *>(user_data);
+    if (GstSample *sample = gst_app_sink_pull_sample(appsink))
+    {
+        hlsOutput->pushSegment(sample);
+    }
+    return GST_FLOW_OK;
+}
+
+static GstFlowReturn tssink_new_sample(GstAppSink *appsink, gpointer user_data)
 {
     if (GstSample *sample = gst_app_sink_pull_sample(appsink)) 
     {
@@ -28,32 +88,58 @@ static GstFlowReturn appsink_new_sample(GstAppSink *appsink, gpointer user_data)
         gst_buffer_unmap(buffer, &mapInfo);
         gst_sample_unref(sample);
     }
+    std::cerr << "ts.";
     return GST_FLOW_OK;
 }
 
 int main(int argc, char *argv[])
 {
-    const int port = 8080;
-    SoupServer *http_server = soup_server_new(SOUP_SERVER_SERVER_HEADER, "HLS Demo ", NULL);
+    HLSOutput hlsOutput;
+
     gst_init(&argc, &argv);
     GstElement *pipeline = gst_pipeline_new(NULL);
     GstElement *videotestsrc = gst_element_factory_make("videotestsrc", NULL);
-    g_object_set(videotestsrc, "num-buffers", 100, NULL);
+    g_object_set(videotestsrc, "is-live", TRUE, NULL);
     GstElement *timeoverlay = gst_element_factory_make("identity", NULL);
     GstElement *videoconvert = gst_element_factory_make("videoconvert", NULL);
     GstElement *h264enc = gst_element_factory_make("vtenc_h264", NULL);
     g_object_set(h264enc, "realtime", TRUE, NULL);
+    GstElement *h264parse = gst_element_factory_make("h264parse", NULL);
+    GstElement *h264tee = gst_element_factory_make("tee", NULL);
+    GstElement *mp4queue = gst_element_factory_make("queue", NULL);
     GstElement *mp4mux = gst_element_factory_make("mp4mux", NULL);
-    g_object_set(mp4mux, "faststart", TRUE, "fragment-duration", 200, NULL);
-    GstElement *sink = gst_element_factory_make("appsink", NULL);
-    g_object_set(sink, "emit-signals", TRUE, "sync", FALSE, NULL);
-    g_signal_connect(sink, "new-sample", G_CALLBACK(appsink_new_sample), NULL);
-    gst_bin_add_many(GST_BIN(pipeline), videotestsrc, timeoverlay, videoconvert, h264enc, mp4mux, sink, NULL);
-    gst_element_link_many(videotestsrc, timeoverlay, videoconvert,  h264enc, mp4mux, sink, NULL);
+    g_object_set(mp4mux, "faststart", TRUE, "fragment-duration", 200, "streamable", TRUE, NULL);
+    GstElement *mp4sink = gst_element_factory_make("appsink", NULL);
+    g_object_set(mp4sink, "emit-signals", TRUE, "sync", FALSE, NULL);
+    g_signal_connect(mp4sink, "new-sample", G_CALLBACK(mp4sink_new_sample), &hlsOutput);
+
+    // GstElement *tsqueue = gst_element_factory_make("queue", NULL);
+    // GstElement *tsmux = gst_element_factory_make("mpegtsmux", NULL);
+    // GstElement *tssink = gst_element_factory_make("appsink", NULL);
+    // g_object_set(tssink, "emit-signals", TRUE, "sync", FALSE, NULL);
+    // g_signal_connect(tssink, "new-sample", G_CALLBACK(tssink_new_sample), NULL);
+
+    gst_bin_add_many(GST_BIN(pipeline), videotestsrc, timeoverlay, videoconvert, h264enc, h264parse, h264tee, NULL);
+    gst_element_link_many(videotestsrc, timeoverlay, videoconvert, h264enc, h264parse, h264tee, NULL);
+    gst_bin_add_many(GST_BIN(pipeline), mp4queue, mp4mux, mp4sink, NULL);
+    gst_pad_link(gst_element_get_request_pad(h264tee, "src_%u"), gst_element_get_static_pad(mp4queue, "sink"));
+    gst_element_link_many(mp4queue, mp4mux, mp4sink, NULL);
+
+    // gst_bin_add_many(GST_BIN(pipeline), tsqueue, tsmux, tssink, NULL);
+    // gst_pad_link(gst_element_get_request_pad(h264tee, "src_%u"), gst_element_get_static_pad(tsqueue, "sink"));
+    // gst_element_link_many(tsqueue, tsmux, tssink, NULL);
+    
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    const int port = 8080;
+    SoupServer *http_server = soup_server_new(SOUP_SERVER_SERVER_HEADER, "HLS Demo ", NULL);
     soup_server_add_handler(http_server, NULL, [](SoupServer *, SoupMessage *msg, const char *, GHashTable *, SoupClientContext *, gpointer)
     {
         set_error_message(msg, SOUP_STATUS_NOT_FOUND);
+    }, NULL, NULL);
+    soup_server_add_handler(http_server, "/api/hack.ts", [](SoupServer *, SoupMessage *msg, const char *, GHashTable *, SoupClientContext *, gpointer)
+    {
+        
     }, NULL, NULL);
     soup_server_add_handler(http_server, "/ui", file_callback, NULL, NULL);
     if (soup_server_listen_all(http_server, port, static_cast<SoupServerListenOptions>(0), 0))
