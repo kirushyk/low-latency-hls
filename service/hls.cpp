5403a9efeaab37d769f789c7f07fbe75dc73ad39
Kyrylo Polezhaiev <kirushyk@gmail.com>
2021-01-22 21:03:25 +0200

    hls: Add #EXT-X-PROGRAM-DATE-TIME

--- service/hls.cpp
+++ service/hls.cpp
@@ -98,12 +98,18 @@ std::string HLSOutput::getPlaylist() const
     ss << "#EXT-X-VERSION:3" << std::endl;
     ss << "#EXT-X-TARGETDURATION:" << SEGMENT_DURATION << std::endl;
     ss << "#EXT-X-MEDIA-SEQUENCE:" << mediaSequenceNumber << std::endl;
-    // gchar *g_date_time_format_iso8601 (GDateTime *datetime);
-    // ss << "#EXT-X-PROGRAM-DATE-TIME:2019-02-14T02:13:36.106Z" << std::endl;
+    bool dateTimeReported = false;
     for (const auto& segment: segments)
     {
         if (segment->finished && segment->data)
         {
+            if (!dateTimeReported)
+            {
+                gchar *formattedDateTime = g_date_time_format_iso8601(segment->dateTime);
+                ss << "#EXT-X-PROGRAM-DATE-TIME:" << formattedDateTime << std::endl;
+                g_free(formattedDateTime);
+                dateTimeReported = true;
+            }
             ss << "#EXTINF:" << segment->duration * 0.000000001 << "," << std::endl;
             ss << "/api/segments/" << segment->number << ".ts" << std::endl;
         }
